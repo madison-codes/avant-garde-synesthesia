5207a5391ee4dce745aff254a4cc80712b0d3474
Madison Kerndt <madison.kerndt@gmail.com>
2016-10-25 13:59:19 -0600

    sound comp.

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { map, extend, reverse } from 'lodash';
 import firebase, { reference, signOut } from '../firebase';
-
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const context = new AudioContext();
-
-const oscillator = context.createOscillator();
-const gain = context.createGain();
-const destination = context.destination;
-const volume = gain.gain;
-
+import Sound from './Sound';
 
 export default class Application extends React.Component {
   constructor() {
@@ -18,25 +10,20 @@ export default class Application extends React.Component {
     };
   }
 
-
-  playAudio() {
-    oscillator.connect(gain);
-    gain.connect(destination);
-    oscillator.start();
-    console.log('hey');
-  }
-
-  pauseAudio() {
-    oscillator.stop();
-    console.log('stop');
-  }
-
   render() {
     return (
       <section>
-        <p>Click the buttons to play or pause the audio.</p>
-        <button onClick={() => this.playAudio()}>Play Audio</button>
-        <button onClick={() => this.pauseAudio()}>Pause Audio</button>
+        <p>
+          Click the buttons to play or pause the audio.
+        </p>
+        <Sound
+          number = { 1 }
+          value = { 440 }
+        />
+        <Sound
+          number = { 2 }
+          value = { 400 }
+        />
       </section>
     );
   }
