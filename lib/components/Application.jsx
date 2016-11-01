0bf44a9283c6370b05a8ed9a71dc5a9061e77e0a
Madison Kerndt <madison.kerndt@gmail.com>
2016-11-01 16:58:47 -0600

    all clean and working

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -10,31 +10,13 @@ export default class Application extends React.Component {
     };
   }
 
-  pauseSound() {
-    gain.gain.value = 0;
-  }
+  // pauseSound() {
+  //   gain.gain.value = 0;
+  // }
 
   render() {
     return (
       <section>
-        <p>
-          Click the buttons to play or pause the audio.
-        </p>
-        {/* <div>
-          <Sound index={ 6 } />
-          <Sound index={ 2 } />
-          <Sound index={ 3 } />
-        </div>
-        <div className='contain-sound'>
-          <Sound index={ 4 } />
-          <Sound index={ 5 } />
-          <Sound index={ 6 } />
-        </div>
-        <div className='contain-sound'>
-          <Sound index={ 7 } />
-          <Sound index={ 8 } />
-          <Sound index={ 9 } />
-        </div> */}
         <TrackColor onNoteChange={note => this.setState({ note })} />
         <Sound note={this.state.note} />
       </section>
