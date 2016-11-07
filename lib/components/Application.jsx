0dfc0096e1bbc752fbbd229448019e49afef0a26
Madison Kerndt <madison.kerndt@gmail.com>
2016-11-06 21:41:42 -0700

    added guiding notes

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -3,6 +3,9 @@ import Sound from './Sound';
 import TrackColor from './TrackColor'
 import Video from './Video';
 import Audio from './Audio';
+import NoteCard from './NoteCard';
+
+import notes from '../helpers/notes';
 
 export default class Application extends React.Component {
   constructor() {
@@ -36,14 +39,43 @@ export default class Application extends React.Component {
           </div>
         </article>
         <article className='directions'>
-          <div className='direction'>
-            <p className='note-name'>#A3</p>
-            <div className='note-location'></div>
-            <div className='line first'> </div>
-            <div className='line second'> </div>
-          </div>
-          <div className='direction'>
-          </div>
+          <NoteCard
+          noteName = {notes()[0] } />
+          <NoteCard
+          noteName = { notes()[1] } />
+          <NoteCard
+          noteName = {notes()[2] } />
+          <NoteCard
+          noteName = {notes()[3] } />
+          <NoteCard
+          noteName = {notes()[4] } />
+          <NoteCard
+          noteName = {notes()[5] } />
+          <NoteCard
+          noteName = {notes()[6] } />
+          <NoteCard
+          noteName = {notes()[7] } />
+          <NoteCard
+          noteName = {notes()[8] } />
+          <NoteCard
+          noteName = {notes()[9] } />
+          <NoteCard
+          noteName = {notes()[10] } />
+          <NoteCard
+          noteName = {notes()[11] } />
+          <NoteCard
+          noteName = {notes()[12] } />
+          <NoteCard
+          noteName = {notes()[13] } />
+          <NoteCard
+          noteName = {notes()[14] } />
+          <NoteCard
+          noteName = {notes()[15] } />
+          <NoteCard
+          noteName = {notes()[16] } />
+
+
+
         </article>
       </section>
     );
